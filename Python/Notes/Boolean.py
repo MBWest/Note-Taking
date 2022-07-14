029172dfaab910e4dcf814b0f9fb75671cae911a
MBWest <MBWest@protonmail.com>
2022-07-13 21:40:49 -0500

    Python Notes (TCM)

--- Python/Notes/Boolean.py
+++
@@ -1,6 +0,0 @@
-# ----------Boolean----------
-# Boolean operators can either be True or False
-
-#True or False
-5 == 5 is True #5 is equal to 5
-6 != 5 is True #6 is not equal to 5
---
+++ Python/Notes/Scanner.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import sys
+import socket
+from datetime import datetime
+
+#Define our target
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+    print("Invalid amount of arguments.")
+    print("Syntax: python3 Scanner.py <ip>")
+
+#Add a pretty banner
+
+print("-" * 50)
+print("Scanning target: "+target)
+print("Time started:"+str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(50,85): #For Loop
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target,port)) #Error indicator. If a port is open it returns '0', if its closed it returns '1'
+        if result == 0:
+            print(f"Port {port} is open")
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit() #Exit program gracefully
+
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit() #Exit program gracefully
+
+except socket.error:
+    print("Could not connect to server.")
+    sys.exit() #Exit program gracefully
